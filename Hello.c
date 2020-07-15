9b256977a739f4a2fecec48886dc4fbddcb3a166
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2020-07-15 17:08:53 +0900

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int showCmd)
+{
+    MessageBox(NULL, "Hello world!", "Hello messagebox", MB_OK);
+
+    return 0;
+}
